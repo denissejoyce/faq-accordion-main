cb4d9f78792e6db0a5fa0346b17a17b2d283daa9
denissejoyce <denisse.alido@gmail.com>
2024-03-25 05:52:10 -0500

    updated card

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
 const expanded = "../assets/images/icon-minus.svg";
 const toexpand = "../assets/images/icon-plus.svg";
+
+document
+  .querySelector(".question")
+  .addEventListener("click", () => alert("hi"));
