2fe8bb2d645830e035bae54ed385ae7c69bb02cc
denissejoyce <denisse.alido@gmail.com>
2024-03-27 16:41:20 -0500

    updated questions
    updated active state for questions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 "use strict";
 
-const toExpand = document.createElement("img");
-toExpand.setAttribute("src", "../assets/images/icon-plus.svg");
+// const toExpand = document.createElement("img");
+// toExpand.setAttribute("src", "../assets/images/icon-plus.svg");
 
-const expandDiv = document.querySelectorAll(".expand");
+// const expandDiv = document.querySelectorAll(".expand");
+
+// const questions = document.querySelectorAll(".expand");
+
+// for (let i = 0; i < questions.length; i++) {
+//  questions[i].appendChild(t oExpand);
+// }
+
+//   toExpand.setAttribute("src", "../assets/images/icon-plus.svg");
+
+const questions = document.querySelectorAll(".expand");
+for (let i = 0; i < questions.length; i++) {
+  const toExpand = document.createElement("img");
+  toExpand.src = "../assets/images/icon-plus.svg";
+  //   toExpand.className = "toExpandIcon";
+  questions[i].appendChild(toExpand);
+}
