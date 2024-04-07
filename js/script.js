7c2b80736dc9cefa1b48db865303a1c08c2c049b
denissejoyce <denisse.alido@gmail.com>
2024-04-07 16:41:15 -0500

    update

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ const questions = document.querySelectorAll(".expand");
 for (let i = 0; i < questions.length; i++) {
   const toExpand = document.createElement("img");
   toExpand.id = "icon-" + (i + 1);
-  if (document.getElementById("a" + (i + 1)).classList.contains("hide")) {
-    toExpand.src = "../assets/images/icon-plus.svg";
-  } else {
+  if (document.getElementById("a" + (i + 1)).classList.contains("open")) {
     toExpand.src = "../assets/images/icon-minus.svg";
+  } else {
+    toExpand.src = "../assets/images/icon-plus.svg";
   }
   questions[i].appendChild(toExpand);
 }
@@ -18,18 +18,18 @@ const clickableQuestions = document.querySelectorAll(".question-clickable");
 for (let i = 0; i < clickableQuestions.length; i++) {
   clickableQuestions[i].addEventListener("click", function () {
     const answer = document.getElementById("a" + (i + 1));
-    answer.classList.toggle("hide");
+    answer.classList.toggle("open");
     // setTimeout(() => {
     //   answer.classList.toggle("hide");
     // }, 1000);
-    if (answer.classList.contains("hide")) {
-      document
-        .getElementById("icon-" + (i + 1))
-        .setAttribute("src", "../assets/images/icon-plus.svg");
-    } else {
+    if (answer.classList.contains("open")) {
       document
         .getElementById("icon-" + (i + 1))
         .setAttribute("src", "../assets/images/icon-minus.svg");
+    } else {
+      document
+        .getElementById("icon-" + (i + 1))
+        .setAttribute("src", "../assets/images/icon-plus.svg");
     }
   });
 }
